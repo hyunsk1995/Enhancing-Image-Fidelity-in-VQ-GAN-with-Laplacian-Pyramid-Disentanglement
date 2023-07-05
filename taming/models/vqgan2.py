c687838dac36d065cd7060c509fe83e83344ddff
hyunsk1995 <hyunskcj@gmail.com>
2023-07-05 13:59:15 +0900

    vq-vae2

--- taming/models/vqgan2.py
+++ taming/models/vqgan2.py
@@ -33,11 +33,11 @@ class HierarchicalVQModel(pl.LightningModule):
         channel = ddconfig["ch"]
         beta = ddconfig["beta"]
 
-        self.encoder_t = Encoder(in_channel, channel, n_res_block, n_res_channel, stride=4)
-        self.encoder_b = Encoder(channel, channel, n_res_block, n_res_channel, stride=2)
+        self.encoder_b = Encoder(in_channel, channel, n_res_block, n_res_channel, stride=4)
+        self.encoder_t = Encoder(channel, channel, n_res_block, n_res_channel, stride=2)
 
         self.quant_conv_t = torch.nn.Conv2d(channel, embed_dim, 1)
-        self.quantize_t = VectorQuantizer(n_embed, embed_dim)
+        self.quantize_t = VectorQuantizer(embed_dim, n_embed)
 
         self.decoder_t = Decoder(
             embed_dim, embed_dim, channel, n_res_block, n_res_channel, stride=2
@@ -90,18 +90,19 @@ class HierarchicalVQModel(pl.LightningModule):
         return dec, diff
 
     def encode(self, input):
-        enc_b = self.enc_b(input)
-        enc_t = self.enc_t(enc_b)
+        enc_b = self.encoder_b(input)
+        enc_t = self.encoder_t(enc_b)
 
-        quant_t = self.quantize_conv_t(enc_t).permute(0, 2, 3, 1)
+        quant_t = self.quant_conv_t(enc_t).permute(0, 2, 3, 1)
+        print(quant_t.shape)
         quant_t, diff_t, id_t = self.quantize_t(quant_t)
         quant_t = quant_t.permute(0, 3, 1, 2)
         diff_t = diff_t.unsqueeze(0)
 
-        dec_t = self.dec_t(quant_t)
+        dec_t = self.decoder_t(quant_t)
         enc_b = torch.cat([dec_t, enc_b], 1)
 
-        quant_b = self.quantize_conv_b(enc_b).permute(0, 2, 3, 1)
+        quant_b = self.quant_conv_b(enc_b).permute(0, 2, 3, 1)
         quant_b, diff_b, id_b = self.quantize_b(quant_b)
         quant_b = quant_b.permute(0, 3, 1, 2)
         diff_b = diff_b.unsqueeze(0)
@@ -111,7 +112,7 @@ class HierarchicalVQModel(pl.LightningModule):
     def decode(self, quant_t, quant_b):
         upsample_t = self.upsample_t(quant_t)
         quant = torch.cat([upsample_t, quant_b], 1)
-        dec = self.dec(quant)
+        dec = self.decoder(quant)
 
         return dec
     
@@ -125,12 +126,12 @@ class HierarchicalVQModel(pl.LightningModule):
 
         return dec
 
-    # def get_input(self, batch, k):
-    #     x = batch[k]
-    #     if len(x.shape) == 3:
-    #         x = x[..., None]
-    #     x = x.permute(0, 3, 1, 2).to(memory_format=torch.contiguous_format)
-    #     return x.float()
+    def get_input(self, batch, k):
+        x = batch[k]
+        if len(x.shape) == 3:
+            x = x[..., None]
+        x = x.permute(0, 3, 1, 2).to(memory_format=torch.contiguous_format)
+        return x.float()
 
     def training_step(self, batch, batch_idx, optimizer_idx):
         x = self.get_input(batch, self.image_key)
@@ -145,30 +146,30 @@ class HierarchicalVQModel(pl.LightningModule):
             self.log_dict(log_dict_ae, prog_bar=False, logger=True, on_step=True, on_epoch=True)
             return aeloss
 
-        # if optimizer_idx == 1:
-        #     # discriminator
-        #     discloss, log_dict_disc = self.loss(qloss, x, xrec, optimizer_idx, self.global_step,
-        #                                     last_layer=self.get_last_layer(), split="train")
-        #     self.log("train/discloss", discloss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
-        #     self.log_dict(log_dict_disc, prog_bar=False, logger=True, on_step=True, on_epoch=True)
-        #     return discloss
+        if optimizer_idx == 1:
+            # discriminator
+            discloss, log_dict_disc = self.loss(qloss, x, xrec, optimizer_idx, self.global_step,
+                                            last_layer=self.get_last_layer(), split="train")
+            self.log("train/discloss", discloss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+            self.log_dict(log_dict_disc, prog_bar=False, logger=True, on_step=True, on_epoch=True)
+            return discloss
 
-    # def validation_step(self, batch, batch_idx):
-    #     x = self.get_input(batch, self.image_key)
-    #     xrec, qloss = self(x)
-    #     aeloss, log_dict_ae = self.loss(qloss, x, xrec, 0, self.global_step,
-    #                                         last_layer=self.get_last_layer(), split="val")
+    def validation_step(self, batch, batch_idx):
+        x = self.get_input(batch, self.image_key)
+        xrec, qloss = self(x)
+        aeloss, log_dict_ae = self.loss(qloss, x, xrec, 0, self.global_step,
+                                            last_layer=self.get_last_layer(), split="val")
 
-    #     discloss, log_dict_disc = self.loss(qloss, x, xrec, 1, self.global_step,
-    #                                         last_layer=self.get_last_layer(), split="val")
-    #     rec_loss = log_dict_ae["val/rec_loss"]
-    #     self.log("val/rec_loss", rec_loss,
-    #                prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=True)
-    #     self.log("val/aeloss", aeloss,
-    #                prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=True)
-    #     self.log_dict(log_dict_ae)
-    #     self.log_dict(log_dict_disc)
-    #     return self.log_dict
+        discloss, log_dict_disc = self.loss(qloss, x, xrec, 1, self.global_step,
+                                            last_layer=self.get_last_layer(), split="val")
+        rec_loss = log_dict_ae["val/rec_loss"]
+        self.log("val/rec_loss", rec_loss,
+                   prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=True)
+        self.log("val/aeloss", aeloss,
+                   prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=True)
+        self.log_dict(log_dict_ae)
+        self.log_dict(log_dict_disc)
+        return self.log_dict
 
     def configure_optimizers(self):
         lr = self.learning_rate
@@ -182,31 +183,31 @@ class HierarchicalVQModel(pl.LightningModule):
                                   list(self.quant_conv_b.parameters()),
                                 #   list(self.post_quant_conv.parameters()),
                                   lr=lr, betas=(0.5, 0.9))
-        # opt_disc = torch.optim.Adam(self.loss.discriminator.parameters(),
-        #                             lr=lr, betas=(0.5, 0.9))
+        opt_disc = torch.optim.Adam(self.loss.discriminator.parameters(),
+                                    lr=lr, betas=(0.5, 0.9))
         return [opt_ae], []
 
-    # def get_last_layer(self):
-    #     return self.decoder.conv_out.weight
+    def get_last_layer(self):
+        return self.decoder.conv_out.weight
 
-    # def log_images(self, batch, **kwargs):
-    #     log = dict()
-    #     x = self.get_input(batch, self.image_key)
-    #     x = x.to(self.device)
-    #     xrec, _ = self(x)
-    #     if x.shape[1] > 3:
-    #         # colorize with random projection
-    #         assert xrec.shape[1] > 3
-    #         x = self.to_rgb(x)
-    #         xrec = self.to_rgb(xrec)
-    #     log["inputs"] = x
-    #     log["reconstructions"] = xrec
-    #     return log
+    def log_images(self, batch, **kwargs):
+        log = dict()
+        x = self.get_input(batch, self.image_key)
+        x = x.to(self.device)
+        xrec, _ = self(x)
+        if x.shape[1] > 3:
+            # colorize with random projection
+            assert xrec.shape[1] > 3
+            x = self.to_rgb(x)
+            xrec = self.to_rgb(xrec)
+        log["inputs"] = x
+        log["reconstructions"] = xrec
+        return log
 
-    # def to_rgb(self, x):
-    #     assert self.image_key == "segmentation"
-    #     if not hasattr(self, "colorize"):
-    #         self.register_buffer("colorize", torch.randn(3, x.shape[1], 1, 1).to(x))
-    #     x = F.conv2d(x, weight=self.colorize)
-    #     x = 2.*(x-x.min())/(x.max()-x.min()) - 1.
-    #     return x
+    def to_rgb(self, x):
+        assert self.image_key == "segmentation"
+        if not hasattr(self, "colorize"):
+            self.register_buffer("colorize", torch.randn(3, x.shape[1], 1, 1).to(x))
+        x = F.conv2d(x, weight=self.colorize)
+        x = 2.*(x-x.min())/(x.max()-x.min()) - 1.
+        return x
